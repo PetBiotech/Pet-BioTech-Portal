33b49aa2ea0067cb50c2dc0c6693db9b2b860807
PetBiotech <116794066+PetBiotech@users.noreply.github.com>
2023-02-20 00:49:20 +0530

    Delete tableFunctions.py

--- tableFunctions.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
