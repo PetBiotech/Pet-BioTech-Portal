f40e5a75ed6b9e661a2f8e820f1de0a9b7dccbe3
Anand <raschoudhary879@gmail.com>
2023-02-23 11:08:34 +0530

    Pdf Changes

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,4 @@
-after_model_change
+if(resultDb.outcome_result=='Positive' or resultDb.outcome_result=='positive' or resultDb.outcome_result=='Pos' or resultDb.outcome_result=='pos'):
+                    resultDb.status=1
+                else:
+                    resultDb.status=0
